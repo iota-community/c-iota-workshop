4ddb79e88497a94487b76aad286e1011edd603aa
JakeSCahill <jake.cahill@iota.org>
2019-12-10 14:49:31 +0000

    Add comments and change message to 'hello world'

--- examples/e02_send_hello.c
+++ examples/e02_send_hello.c
@@ -2,12 +2,11 @@
 #include "cclient/api/extended/extended_api.h"
 
 #include "common/trinary/tryte_ascii.h"
-//#include "utils/time.h"
 #include <inttypes.h>
 #include "iota_client_service/config.h"
 #include "iota_client_service/client_service.h"
 
-static tryte_t const *const RECEIVER_ADDR =
+static tryte_t const *const ADDRESS =
 (tryte_t *)"TOKLOARHKXQCVPPVVIPIJGLUTLTKFHYGMBBLOXJFYGSARLOTYFFSDZNYCOBOCNPGRMJWZCQBNOROUCE9G";
 
 static tryte_t const *const TAG =
@@ -17,41 +16,44 @@ static tryte_t const *const TAG =
 retcode_t send_transaction(iota_client_service_t *service){
     retcode_t ret_code = RC_OK;
 
-    char message[] = "You did it!";
+    char message[] = "Hello world";
 
     bundle_transactions_t *bundle = NULL;
     bundle_transactions_new(&bundle);
+    // Create a transfers array to which you can add transaction data
     transfer_array_t *transfers = transfer_array_new();
 
     transfer_t tf = {};
 
-    // Convert the trytes to trits
+    // Convert the address from trytes to trits
     // For more information about trits and trytes, see the IOTA documentation portal: https://docs.iota.org/docs/getting-started/0.1/introduction/ternary
     flex_trits_from_trytes(tf.address, NUM_TRITS_ADDRESS, RECEIVER_ADDR, NUM_TRYTES_ADDRESS, NUM_TRYTES_ADDRESS);
 
-    //Getting trits from tag chars
+    // Convert the tag from trytes to trits
     flex_trits_from_trytes(tf.tag, NUM_TRITS_TAG, (const tryte_t *)TAG, NUM_TRYTES_TAG,
                            NUM_TRYTES_TAG);
 
-    //Add the ASCII message to the transfer
+    //Add the ASCII message to array
     transfer_message_set_string(&tf, message);
 
-    // Prepare the transfer array
+    // Create a bundle from the transfers array
     transfer_array_add(transfers, &tf);
 
-    //Send transfer and setting seed = NULL, because we are sending a zero value transaction
+    //Send the bundle and set the seed to NULL, because you are sending a zero value transaction
     ret_code = iota_client_send_transfer(
-            service, NULL, SECURITY_LEVEL, TIP_SELECTION_DEPTH,
+            service, NULL, SECURITY_LEVEL, DEPTH,
             MINIMUM_WEIGHT_MAGNITUDE, false, transfers, NULL, NULL, NULL, bundle);
 
-    printf("send transfer %s\n", error_2_string(ret_code));
+    printf("Send \'hello world\' %s\n", error_2_string(ret_code));
     if (ret_code == RC_OK) {
         flex_trit_t const *bundle_hash = bundle_transactions_bundle_hash(bundle);
-        printf("bundle hash: ");
+        printf("Bundle hash: ");
+        // Print the bundle hash to the console
         flex_trit_print(bundle_hash, NUM_TRITS_HASH);
         printf("\n");
     }
 
+    // Free the objects
     bundle_transactions_free(&bundle);
     transfer_message_free(&tf);
     transfer_array_free(transfers);
@@ -68,6 +70,6 @@ int main(void){
     if(ret == RC_OK){
         printf("Transaction was sent.\n");
     }else{
-        printf("Transaction failed with error code: %i\n", ret);
+        printf("Transaction failed to send with error code: %i\n", ret);
     }
 }
